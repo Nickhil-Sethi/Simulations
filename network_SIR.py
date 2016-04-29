1a63f09e76205be19e71a4b49d974fe52d1839dc
Nickhil-Sethi <ns1635@nyu.edu>
2016-04-28 23:07:31 -0400

    deleted functions

--- network_SIR.py
+++ network_SIR.py
@@ -16,69 +16,6 @@ nodes.
 
 '''
 
-# this always iterates through N nodes
-def dynamics(N,state,adj):
-    for v in xrange(N):
-        if state[v] == 1:
-            for w in adj[v]:
-                if state[w] == 0:
-                    o = np.random.rand()
-                    if o < alpha:
-                        state[w] = 1
-            y = np.random.rand()
-            if y < beta:
-                state[v] = 2
-    return state
-
-# this only iterates through 'active nodes'
-def dynamics_2(N,state,active,adj,remove_stack):
-    # iteration through active nodes is O(n)
-
-    L = active.size
-    counter = 0
-    while not active.is_empty() and counter < L:
-        counter += 1
-        v = active.dequeue()
-        # action on neighborhood is O(m)
-        for w in adj[v]:
-            # O(1)
-            if state[w] == 0:
-                o = np.random.rand()
-                if o < alpha:
-                    state[w] = 1
-                    active.enqueue(w)        
-        # removal of i from is O(n)
-        y = np.random.rand()
-        if y < beta:
-            state[v] = 2
-        else:
-            active.enqueue(v)
-    return state
-
-def simulation_1(N=100,time=100,graph='random',delta=.5, w=.5,p=.5,dt=.01,alpha_raw=1.0,beta_raw=1.0):
-    graphs = ['random','scale free','small world']
-
-    if not graph in graphs:
-        raise ValueError('graph must be type [random, scale free, small world]')
-
-    global alpha, beta
-    alpha = alpha_raw*dt 
-    beta = beta_raw*dt
-    state=np.zeros(N)
-    state[0]=1
-
-    if graph == 'random':
-        adj = construct_random_graph(N,delta)
-    elif graph == 'scale free':
-        adj = construct_scale_free_graph(N,w)
-    else:
-        adj = construct_small_world_graph(N,p)
-
-    for i in range(time):                      
-        state=dynamics(N,state,adj)
-
-    return state
-
 def simulation_2(N=100,time=100,graph='random',delta=.1, w=.5,p=.5,dt=.01,alpha_raw=1.0,beta_raw=1.0):
 
     # sanity checks
